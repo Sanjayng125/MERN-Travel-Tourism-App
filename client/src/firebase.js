bf816f17960396897baa6ed0dbe12483c9db6b10
Sanjayng <sanjayng001@gmail.com>
2024-12-19 12:24:00 +0530

    Update

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,19 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { firebaseConfig } from "./firebaseConfig";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// Example
-// const firebaseConfig = {
-//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//   authDomain: "",
-//   projectId: "",
-//   storageBucket: "",
-//   messagingSenderId: "",
-//   appId: "",
-// };
+//temporary souls firebase
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-travel-tourism.firebaseapp.com",
+  projectId: "mern-travel-tourism",
+  storageBucket: "mern-travel-tourism.appspot.com",
+  messagingSenderId: "304378877141",
+  appId: "1:304378877141:web:3bddab2778ca23186f7e5c",
+};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
